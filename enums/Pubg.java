0e1b9f789b01d0960e85f1530e33bb9f0c31a1e2
Shriharsha K M <shriharshakm10@gmail.com>
2025-03-10 22:45:22 +0530

    uploding files

---
+++ enums/Pubg.java
@@ -0,0 +1,6 @@
+class Pubg{
+	public static void Gun( GunSelect gunname){
+		System.out.println("running on pubg"+gunname);
+		
+	}
+}
---
+++ enums/PubgRunner.java
@@ -0,0 +1,6 @@
+class PubgRunner{
+	public static void main(String[] values){
+		GunSelect gunname=GunSelect.UZI;
+		Pubg.Gun(gunname);
+	}
+}
