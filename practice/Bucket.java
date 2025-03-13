cd1a26f88d1cc53f1997221a7c256686681b4dac
Shriharsha K M <shriharshakm10@gmail.com>
2025-03-14 00:40:11 +0530

    updating daily assignment

---
+++ practice/Bucket.java
@@ -0,0 +1,16 @@
+class Bucket{
+	String name="harsha";
+	
+	
+	//non static is instance 
+	// static- static ok
+	// instance-static ok
+	// static-instance not ok
+	// instance-instance ok
+	
+	public  void jug(String name){
+		
+		this.name=name 
+		
+	}
+}
---
+++ practice/BucketRunner.java
@@ -0,0 +1,8 @@
+class BucketRunner{
+	public static void main(String[] values){
+		Bucket bucket = new Bucket();
+		bucket.jug();
+		// with ref - instance variable/method
+		// Class name - Static methods 
+	}
+}
