886bdc8ec1cd7b2caae0143499b06431d2cce84c
Shriharsha K M <shriharshakm10@gmail.com>
2025-03-16 00:24:42 +0530

    updating assignment

--- Constructor and this chaining/Ballon.java
+++ Constructor and this chaining/Ballon.java
@@ -3,13 +3,21 @@ class Ballon{
 	int age;
 	
 	
+	public Ballon(){
+		System.out.println("no args constructor");
+	}
 	
 	
 	public Ballon(String name){
+		this();
 		this.name=name;
 	}
 	public Ballon(String name,int age){
-		this(Ballon);
+		this(name);
 		this.age=age;
+	}
+	public static void disply(){
+		
+		System.out.println(this.name+this.age);
 	}
 }
--- Constructor and this chaining/MainRunner.java
+++ Constructor and this chaining/MainRunner.java
@@ -1,1 +1,10 @@
-class MainRunner{}
+class MainRunner{
+	public static void main(String[] values){
+	String name="huh";
+		int age=45;
+		Ballon ballon=new Ballon(name);
+		Ballon ballon1=new Ballon(name,age);
+		ballon.disply();
+		ballon1.disply();
+	}
+}
